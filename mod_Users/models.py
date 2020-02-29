f6917f60672fec160c2a87c4b19495741a990034
fatemehloni <fatemehloni@gmail.com>
2020-02-29 12:52:39 +0330

    add check_password_hash to models

--- mod_Users/models.py
+++ mod_Users/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import  Integer, String,Column
 from app import db
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 
@@ -14,3 +14,7 @@ class User(db.Model):
 
     def set_password(self,password):
         self.password=generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password, password)
+
