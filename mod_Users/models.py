59a294528e90f91ebf47848bdc55db38fb9b1e42
fatemehloni <fatemehloni@gmail.com>
2020-03-04 15:23:53 +0330

    Update

--- mod_Users/models.py
+++ mod_Users/models.py
@@ -1,20 +1,21 @@
-from sqlalchemy import  Integer, String,Column
+from sqlalchemy import Integer, String, Column
 from app import db
-from werkzeug.security import generate_password_hash,check_password_hash
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
-    __tablename__='users'
+    __tablename__ = 'users'
     id = Column(Integer(), primary_key=True)
     email = Column(String(128), nullable=False, unique=True)
     password = Column(String(128), nullable=False, unique=False)
-    role = Column(Integer(), nullable=False, default=0)
+    role = Column(Integer(), nullable=False, default=0)  # 0:user 1:admin
     fullname = Column(String(128), nullable=True, unique=False)
 
-    def set_password(self,password):
-        self.password=generate_password_hash(password)
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
 
-    def check_password(self,password):
+    def check_password(self, password):
         return check_password_hash(self.password, password)
 
+    def is_admin(self):
+        return self.role == 1
