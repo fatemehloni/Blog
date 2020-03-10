3036c06a54f0569757798d07c766f2b9fa4f3e1c
fatemehloni <fatemehloni@gmail.com>
2020-03-10 16:00:05 +0330

    Update

--- mod_blog/form.py
+++ mod_blog/form.py
@@ -1,21 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, TextAreaField
+from wtforms import TextField, TextAreaField, SelectMultipleField
 from wtforms.validators import DataRequired
+from Utils.forms import MultipleCheckboxField
 
 
-class CreatePostForm(FlaskForm):
+class PostForm(FlaskForm):
     title = TextField(validators=[DataRequired()])
     summary = TextAreaField()
     content = TextAreaField(validators=[DataRequired()])
     slug = TextField(validators=[DataRequired()])
+    categories = MultipleCheckboxField()
 
-class ModifyPostForm(FlaskForm):
-    title = TextField(validators=[DataRequired()])
-    summary = TextAreaField()
-    content = TextAreaField(validators=[DataRequired()])
-    slug = TextField(validators=[DataRequired()])
 
 class CategoryForm(FlaskForm):
     name = TextField(validators=[DataRequired()])
-    slug =TextField(validators=[DataRequired()])
+    slug = TextField(validators=[DataRequired()])
     description = TextAreaField()
